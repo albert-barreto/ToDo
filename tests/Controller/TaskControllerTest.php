7d991d93bf830d2ec73dc611450ffd3ecfbf0d50
Albert Barreto <albert.barreto@outlook.com>
2020-12-12 18:03:13 +0100

    logger and unit test

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -23,4 +23,21 @@ class TaskControllerTest extends WebTestCase
 
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
     }
+
+    public function testCreate(): void
+    {
+        $client = static::createClient();
+
+        $client->request('POST', '/tasks/', ['title' => 'test']);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testUpdate(): void
+    {
+        $client = static::createClient();
+        $client->request('PUT', '/tasks/1', ['title' => 'test']);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
 }
